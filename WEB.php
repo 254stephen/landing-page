bb30bd21668d911df58d11f16d4dd0ee8fb57b27
254stephen <108427740+254stephen@users.noreply.github.com>
2023-01-25 10:57:20 +0300

    Add files via upload

---
+++ WEB.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="STYLESHEET" type="text/css" href="WEB.CSS">
+</head>
+<body>
+    <div class="header">
+       <NAV>
+            <div class="logo">
+            <!--<img src="images/20172F112F302F152F442F132Fb80d6f21-5098-41d0-a912-faf50e603a182FCookies.png">-->
+                <span style="color: chartreuse; font-size: 70px;">P</span><span style="color: gold;font-size: 64px;">emak</span>
+            </div>
+            <ul>
+                <li><a href="#">HOME</a></li>
+                <li><a href="#">FEATURES</a></li>
+                <li><a href="#">FITNESS</a></li>
+                <li><a href="#">CONNECTIVITY</a></li>
+                <li><a href="#">SUPPORT</a></li>
+            </ul>
+       </NAV>  
+       <div class="content">
+           <h1 class="slide-left">The<br>Future</h1>
+           <p class="slide-left">This is the first smart watch from our company.we're making good watches 
+               for all the categories</p>
+               <div class="links slide-left">
+                   <a href="#" class="btn">Learn More or</a>
+                   <a href="#" class="btn">login here</a>
+               </div>
+       </div>
+
+    </div>
+</body>
+</html>
